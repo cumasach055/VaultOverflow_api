2020c8fd242f9dfc2eae404a160f41acbe80fac8
Timofey Biryukov <timofeybiryukov@gmail.com>
2019-11-11 20:40:43 +0600

    cors

--- config/fixtures.js
+++ config/fixtures.js
@@ -3,14 +3,41 @@
 module.exports.fixtures = {
   order: ['Question'],
   Question: [
-    {title: 'Hello', description: 'world'},
-    {title: 'Hello', description: 'world'},
-    {title: 'Hello', description: 'world'},
-    {title: 'Hello', description: 'world'},
-    {title: 'Hello', description: 'world'},
-    {title: 'Hello', description: 'world'},
-    {title: 'Hello', description: 'world'},
-    {title: 'Hello', description: 'world'},
-    {title: 'Hello', description: 'world'}
+    {
+      'title': 'magna duis magna aute laborum',
+      'description': 'Cillum magna excepteur excepteur reprehenderit dolore et in cupidatat minim magna ipsum sint. Anim ad enim laboris culpa laborum minim. Ullamco mollit tempor laboris sint ullamco ut officia et est commodo excepteur irure commodo.'
+    },
+    {
+      'title': 'sit amet tempor reprehenderit aliquip',
+      'description': 'Esse nostrud aliquip laborum magna enim eu ut. Nostrud voluptate cupidatat occaecat velit magna dolor est aute. Minim tempor consectetur enim do minim. Et cupidatat anim culpa consectetur. Lorem dolore exercitation commodo ex ea voluptate eu aliquip eiusmod exercitation.'
+    },
+    {
+      'title': 'veniam sunt officia commodo labore',
+      'description': 'Occaecat aliquip sunt dolor pariatur amet aliquip proident pariatur adipisicing tempor in ea irure. Excepteur aliqua eiusmod nisi ex amet ea excepteur. Sint et esse ullamco veniam magna nisi minim qui et id ea veniam aliquip. Est dolore duis magna occaecat elit sunt consectetur ipsum. Duis et et nulla quis minim eu anim eiusmod esse qui exercitation sit dolor. Aliqua aliqua cillum in id fugiat velit sint dolore proident voluptate aliquip. Do pariatur cillum deserunt quis minim ea eu reprehenderit nulla dolore est proident cillum in.'
+    },
+    {
+      'title': 'mollit aute nulla dolore elit',
+      'description': 'Qui aliqua qui ea exercitation enim amet labore excepteur quis velit et. Do culpa adipisicing excepteur consectetur ea pariatur adipisicing veniam proident commodo ullamco magna sunt. Cillum laborum ullamco non aliqua enim ipsum ad proident Lorem. Dolore tempor do sint incididunt pariatur ipsum duis amet aute non.'
+    },
+    {
+      'title': 'elit dolore laboris irure irure',
+      'description': 'Nostrud nostrud ad aliquip occaecat nostrud non veniam fugiat nostrud incididunt in. Ex magna occaecat proident ut laboris in consequat aliqua incididunt excepteur. Ut exercitation est culpa commodo officia cupidatat duis velit culpa velit dolore in. Pariatur culpa eu quis nostrud duis non. Ex sunt nisi minim Lorem id minim veniam cupidatat ad cillum magna culpa ea. Adipisicing et dolor ullamco elit anim excepteur dolor in nisi reprehenderit in do eu. Quis aliqua enim do irure cupidatat esse tempor ea.'
+    },
+    {
+      'title': 'minim commodo magna quis sit',
+      'description': 'Nisi sit qui sint enim officia officia esse ex aliqua nisi labore magna. Esse reprehenderit quis ea duis occaecat labore cillum excepteur exercitation Lorem sit ut nostrud. Consequat id sint quis tempor officia incididunt labore consequat.'
+    },
+    {
+      'title': 'voluptate non do ad consectetur',
+      'description': 'Nulla labore proident magna mollit dolore eiusmod voluptate exercitation. Laboris consequat veniam et minim minim magna veniam nostrud laboris officia aliqua Lorem. Excepteur consequat non duis nulla aliqua occaecat commodo do enim eu mollit amet proident. Minim occaecat cillum officia irure pariatur ex id consequat. Laborum eu consectetur quis nostrud enim laborum deserunt culpa duis tempor ullamco ipsum id ea. Quis laborum esse labore eu aliquip excepteur nisi. Ullamco nisi id voluptate tempor ea magna irure sit.'
+    },
+    {
+      'title': 'ea enim reprehenderit aute cupidatat',
+      'description': 'Deserunt tempor laborum anim cillum velit dolor. Velit mollit laboris id sit irure officia et ipsum dolor reprehenderit consequat sunt consequat non. Lorem cupidatat sunt laboris incididunt deserunt enim nostrud. In ut voluptate minim ex Lorem laborum ipsum aute in irure. Ea proident ea pariatur ut duis nostrud irure laborum voluptate aliqua nostrud nulla. Magna duis magna laborum amet ut eiusmod elit quis aliqua veniam. Nostrud deserunt eu dolor magna nostrud.'
+    },
+    {
+      'title': 'duis elit incididunt occaecat commodo',
+      'description': 'Reprehenderit occaecat mollit tempor est ad dolor consequat tempor commodo elit ad. Ex cupidatat non nostrud minim ullamco reprehenderit elit reprehenderit culpa occaecat sit quis do sunt. Cillum tempor ullamco in adipisicing tempor dolor pariatur est quis. Eu do labore reprehenderit ullamco officia id sit id. Irure cupidatat incididunt officia reprehenderit officia proident sit magna ex et.'
+    }
   ]
 };
